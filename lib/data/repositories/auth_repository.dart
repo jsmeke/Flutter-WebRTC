508f5b2248d91b60f2bd9270e44f2e30bd60d31c
Damian Sagranichne <dsagra@gmail.com>
2023-08-03 14:08:02 -0300

    fres

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:video_streaming/data/datasources/remote_datasource.dart';
 import 'package:video_streaming/domain/repositories/auth_repository.dart';
-import 'package:video_streaming/utils/logger.dart';
 
 class AuthRepository implements AuthRepositoryInt {
   final RemoteDataSource _remoteDatasource;
@@ -10,27 +8,27 @@ class AuthRepository implements AuthRepositoryInt {
 
   @override
   Future<void> signInAnonymously() async {
-    try {
-      final userCredential = await FirebaseAuth.instance.signInAnonymously();
-      _remoteDatasource.userId = userCredential.user?.uid;
-    } on FirebaseAuthException catch (e) {
-      switch (e.code) {
-        case 'operation-not-allowed':
-          Logger.printRed(
-            message: 'Anonymous auth hasn\'t been enabled for this project',
-            filename: 'auth_repository',
-            method: 'signInAnonymously',
-            line: 18,
-          );
-          break;
-        default:
-          Logger.printRed(
-            message: 'Unknown error',
-            filename: 'auth_repository',
-            method: 'signInAnonymously',
-            line: 26,
-          );
-      }
-    }
+    //   try {
+    //     final userCredential = await FirebaseAuth.instance.signInAnonymously();
+    //     _remoteDatasource.userId = userCredential.user?.uid;
+    //   } on FirebaseAuthException catch (e) {
+    //     switch (e.code) {
+    //       case 'operation-not-allowed':
+    //         Logger.printRed(
+    //           message: 'Anonymous auth hasn\'t been enabled for this project',
+    //           filename: 'auth_repository',
+    //           method: 'signInAnonymously',
+    //           line: 18,
+    //         );
+    //         break;
+    //       default:
+    //         Logger.printRed(
+    //           message: 'Unknown error',
+    //           filename: 'auth_repository',
+    //           method: 'signInAnonymously',
+    //           line: 26,
+    //         );
+    //     }
+    //   }
   }
 }
